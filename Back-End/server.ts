97a833d47a8f9ddaf2ff92cf72ca71b187abdea0
ehrvayn <rjmomong@gmail.com>
2026-03-24 13:31:43 +0800

    Fix: fix api call

--- Back-End/server.ts
+++ Back-End/server.ts
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://navsumaro.vercel.app"],
+    origin: ["http://localhost:5173", "https://navsumaro.onrender.com"],
     credentials: true,
   },
 });
